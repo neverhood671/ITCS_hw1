694cadc7c8285d8705952ff0ede475b661984e8b
Настя <nastya-art@yandex.ru>
2017-11-28 10:01:42 +0300

    add average value calculation and drawing min, max and average

--- mysim.py
+++ mysim.py
@@ -8,7 +8,7 @@ def k_base_to_decimal(val, base):
     index = len(val)
     for j in val:
         index -= 1
-        res += j * pow(base, index)
+        res += j * (base ** index)
     return res
 
 
@@ -20,6 +20,9 @@ class MySim(Model):
         self.rule = self.build_rule_set(rule_name, base)
         self.initial_raw = initial_raw
         self.current_raw = initial_raw
+        rule_mas_dimension = 2 * neighbors_num + 1
+        self.rule_mas = np.zeros(([base] * rule_mas_dimension), dtype=np.int)
+        self.build_rule_mas(self.rule_mas, rule_mas_dimension, len(self.rule) - 1)
 
     def reset(self):
         pass
@@ -34,8 +37,15 @@ class MySim(Model):
         prev_state.insert(0, prev_state[-1])
         prev_state.append(prev_state[1])
         while start < length:
-            current_val = prev_state[start:end:1]
-            self.current_raw.append(self.rule[rule_length - k_base_to_decimal(current_val, self.k) - 1])
+            # current_val = prev_state[start:end:1]
+            # self.current_raw.append(self.rule[rule_length - k_base_to_decimal(current_val, self.k) - 1])
+            sub_rule_mas = self.rule_mas[prev_state[start]]
+            array_index = start + 1
+            while array_index < end:
+                sub_rule_mas = sub_rule_mas[prev_state[array_index % length]]
+                array_index += 1
+
+            self.current_raw.append(sub_rule_mas)
             start += 1
             end += 1
 
@@ -56,3 +66,15 @@ class MySim(Model):
 
     def check_rule(self, prev_state_with_neighbors):
         return self.rule[k_base_to_decimal(prev_state_with_neighbors, self.k)]
+
+    def build_rule_mas(self, rule_mas, dimension, m):
+        i = 0
+        next_dimension = dimension - 1
+        while i < self.k:
+            if dimension > 1:
+                m = self.build_rule_mas(rule_mas[i], next_dimension, m)
+            else:
+                rule_mas[i] = self.rule[m]
+                m -= 1
+            i += 1
+        return m
